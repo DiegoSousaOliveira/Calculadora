6adb621d75da0fad2dd164957134a68b808a7709
Diego Sousa <diegosousa0056@gmail.com>
2024-01-26 12:52:26 -0300

    variáveis  padrão

--- variables.py
+++ variables.py
@@ -3,3 +3,9 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 FILES_DIR = ROOT_DIR / 'files'
 WINDOW_ICON_PATH = FILES_DIR / 'icon.png'
+
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 19
+TEXT_MARGIN = 10
+MINIMUM_WIDTH = 500

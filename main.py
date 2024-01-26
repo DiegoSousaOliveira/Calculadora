85089da61ba690f4c8a02845b2d535b04813b59b
Diego Sousa <diegosousa0056@gmail.com>
2024-01-26 12:56:43 -0300

    colocando um display

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from main_window import MainWindow
+from display import Display
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
@@ -13,6 +14,10 @@ if __name__ == '__main__':
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    display = Display()
+    window.addToVLayout(display)
+
     window.adjustFixedSize()
     window.show()
     app.exec()
+    
